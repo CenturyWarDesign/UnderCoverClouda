c9caf7db06ec121d2e3ae4043e9cf114d36bd84b
wanbin <wanbin89731@163.com>
2014-01-26 23:09:50 +0800

    base

---
+++ app/server_config/bae.js
@@ -0,0 +1,17 @@
+//config file for bae
+if(sumeru.BAE_VERSION){
+  sumeru.config.database({
+    dbname : 'XqFbPBtJpxXxGJmfSiYG',
+    user: 'Te9PTccuFbyPYmPRHTrq7sNV',//bae 3.0 required
+    password: 'nERdAvmTE2phT3pd8eA4Wjzd9lrvH8O8',//bae 3.0 required
+  }); 
+  sumeru.config({
+    site_url : 'century.duapp.com', //with tailing slash
+  });
+  sumeru.config.cluster({
+      enable : false,
+      dbname : 'rYByQnBTvxhhlAmlSkDX',
+      user: 'Te9PTccuFbyPYmPRHTrq7sNV',
+      password: 'nERdAvmTE2phT3pd8eA4Wjzd9lrvH8O8',
+  });
+}
