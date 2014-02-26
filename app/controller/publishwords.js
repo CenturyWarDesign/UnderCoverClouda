30c9dcd55e6844046c9ee37a1210b512e0de6439
wanbin <wanbin89731@163.com>
2014-02-26 23:28:05 +0800

    提交卧底词汇功能完成

--- app/controller/publishwords.js
+++ app/controller/publishwords.js
@@ -12,7 +12,7 @@ App.publishwords = sumeru.controller.create(function(env, session) {
 	env.onrender = function(doRender) {
 		doRender("publishwords", ['none', 'z']);
 	};
-
+	var count=5;
 
 	env.onready = function() {
 		touch.on(document.getElementById('back'), 'tap', returnlast);
@@ -20,19 +20,36 @@ App.publishwords = sumeru.controller.create(function(env, session) {
 		// $('#change').modal(options)
 		// $('#myModal').modal('toggle');
 		// console.log(getPublishWrod(1));
-		for(var i=1;i<=10;i++){
+		for (var i = 1; i <= count; i++) {
 			$("#content_words").append(getPublishWrod(i));
 		}
 	};
 
-	var sendPunish= function(){
-		alert("提交成功");
-		return;
-		var postData = {
-			"content": $("#content").val(),
-			"type": 1
+	var sendPunish = function() {
+		var sendwords = {};
+		console.log(sendwords);
+		for (var i = 1; i <= count; i++) {
+			var words1 = $("#words1_" + i).val();
+			var words2 = $("#words2_" + i).val();
+			var type = 1;
+			if (words1 != "" && words2 != "") {
+				var temsend = {
+					"words1": words1,
+					"words2": words2,
+					"type": type
+				};
+				sendwords[i]=temsend;
+			}
+
+			// console.log(words1+"_"+words2);
 		}
-		var url = Library.base.getUrl("PublishNew",postData);
+		console.log(sendwords);
+
+
+		var postData = {
+			"data": sendwords,
+		}
+		var url = Library.base.getUrl("WordsNew", postData);
 		console.log(url);
 		sumeru.external.get(url, sendSuccess);
 		// returnlast();
@@ -42,14 +59,15 @@ App.publishwords = sumeru.controller.create(function(env, session) {
 		history.go(-1);
 
 	}
-	var sendSuccess=function(data){
+	var sendSuccess = function(data) {
 		console.log(data);
+		alert("发布成功");
 		returnlast();
 	}
 
 
-	var getPublishWrod=function(id){
-		return '<div class="form-group form-horizontal" style="padding:10px"> <label for="inputEmail3" class="col-sm-1 control-label">词汇'+id+':</label> <div class="col-sm-4"> <input type="text" class="form-control col-sm-10" placeholder="Text input"> </div> <div class="col-sm-4"> <input type="text" class="form-control col-sm-10" placeholder="Text input"> </div> </div> <div class="clearfix"></div>';
+	var getPublishWrod = function(id) {
+		return '<div class="form-group form-horizontal" style="padding:10px"> <label for="inputEmail3" class="col-sm-1 control-label">词汇' + id + ':</label> <div class="col-sm-4"> <input type="text" class="form-control col-sm-10" placeholder="Text input" id=words1_' + id + '> </div> <div class="col-sm-4"> <input type="text" class="form-control col-sm-10" placeholder="Text input" id=words2_' + id + '> </div> </div> <div class="clearfix"></div>';
 	}
 
 });
